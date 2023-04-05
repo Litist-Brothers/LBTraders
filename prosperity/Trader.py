66c0836201ec0f6597bd1e5a05ae7dc70ced452b
Yuvraj Doshi <98044753+YuvrajDoshi01@users.noreply.github.com>
2023-04-05 22:43:21 +0530

    final files along with a script to back test the data

--- prosperity/Trader.py
+++ prosperity/Trader.py
@@ -5,10 +5,6 @@
 
 from typing import Dict, List
 from datamodel import OrderDepth, TradingState, Order, Trade
-import numpy as np
-
-expenditure = 0
-gained = 0
 
 class Trader:
 
@@ -18,37 +14,110 @@ class Trader:
         and outputs a list of orders to be sent
         """
         result = {}
-        
-        print(state.toJSON())
         for product in state.order_depths.keys():
-            if product == 'PEARLS':
                 order_depth : OrderDepth = state.order_depths[product]
                 orders : List[Order] = []
-                market_trades : Trade = state.market_trades[product]
+                # market_trades : Trade = state.market_trades[product]
+                # order_dict = dict()
+                # order_dict = {
+                #     'BAGUETTE':1,
+                #     'BANANAS':2,
+                #     'BERRIES':3,
+                #     'COCONUTS':4,
+                #     'DIP':5,
+                #     'DIVING_GEAR':6,
+                #     'PEARLS':7,
+                #     'PICNIC_BASKET':8,
+                #     'PINA_COLADAS':9,
+                #     'UKULELE':10,
+                # }
                 # a fair value for pearls, calculate this afterwords
                 # acceptable_price = 1
-                acceptable_price = fair_value(market_trades)
-                if len(order_depth.sell_orders) >0 :
+                acceptable_price = calculate_acceptable_price(product)
+                order_limit = calculate_order_limit(product)
+                profit = 0
+                total_sell = 0
+                total_buy = 0
+                # k = list()
+                # i = order_dict.get_value(product)
+
+                # for j in range(1,10):
+                #     k[j] = list(dict())
+
+
+
+                if len(order_depth.sell_orders) >0:
                     best_ask = min(order_depth.sell_orders.keys())
                     best_ask_volume = order_depth.sell_orders[best_ask]
 
 
-                if best_ask < acceptable_price :
-                    print("BUY", str(-best_ask_volume)+ "x", best_ask)
-                    orders.append(Order(product, best_ask,-best_ask_volume))
+                    if best_ask < acceptable_price[0] :
+                        # print("BUY", str(-best_ask_volume)+ "x", best_ask)
+                        # dict = {
+                        #      best_ask:best_ask_volume
+                        #      }
+                        # k[i].append(dict)
+                        orders.append(Order(product, best_ask,-best_ask_volume))
+                        total_buy += best_ask_volume*best_ask
+                        #print(orders)
 
                 if len(order_depth.buy_orders) != 0:
                     best_bid = max(order_depth.buy_orders.keys())
                     best_bid_volume = order_depth.buy_orders[best_bid]
-                    if best_bid > acceptable_price:
-                        print("SELL", str(best_bid_volume) + "x", best_bid)
+                    if best_bid > (acceptable_price[0]+ acceptable_price[1])/2:
+                        # print("SELL", str(best_bid_volume) + "x", best_bid)                        
                         orders.append(Order(product, best_bid, -best_bid_volume))
-
+                        total_sell+= best_bid_volume*best_bid
                 result[product] = orders
+                profit += total_sell - total_buy
+                
+        # print(f"profit is {profit}")
 
         return result
+    
 
 
-def fair_value(market_trades):
-    prices = [trade.price for trade in market_trades]
-    return np.average(prices)
+def calculate_acceptable_price(product):
+    if product == 'PEARLS': 
+        return [9999.764343,10000.71712]
+    elif product == 'BANANAS':
+        return [4808.374679,4853.024026]
+    elif product == 'COCONUTS':
+        return [7919.869686,7984.942623]
+    elif product == 'PINA_COLADAS':
+        return [14869.5898,14980.09281]
+    elif product == 'BERRIES':
+        return [3890.842107,3926.403546]
+    elif product == 'DIVING_GEAR':
+        return [99216.09501,99745.53553]
+    elif product =='PICNIC_BASKET':
+        return [73954.66535,74098.13963]
+    elif product =='UKULELE':
+        return [20718.56719,21020.86239]
+    elif product =='BAGUETTE':
+        return [12106.66216,12326.19219]
+    elif product == 'DIP':
+        return [7080.569267,7092.061485]
+        
+
+def calculate_order_limit(product):
+    if product == 'PEARLS': 
+        return 20
+    elif product == 'BANANAS':
+        return 20
+    elif product == 'COCONUTS':
+        return 600
+    elif product == 'PINA_COLADAS':
+        return 300
+    elif product == 'BERRIES':
+        return 250
+    elif product == 'DIVING_GEAR':
+        return 50
+    elif product =='PICNIC_BASKET':
+        return 70
+    elif product =='UKULELE':
+        return 70
+    elif product =='BAGUETTE':
+        return 150
+    elif product == 'DIP':
+        return 300
--- prosperity/datamodel.py
+++ prosperity/datamodel.py
@@ -1,6 +1,8 @@
 import json
-from typing import Dict, List
 from json import JSONEncoder
+from typing import Dict, List
+
+# Data model found at https://imc-prosperity.notion.site/Writing-an-Algorithm-in-Python-c44b46f32941430fa1eccb6ff054be26
 
 Time = int
 Symbol = str
@@ -28,22 +30,28 @@ class Order:
 
     def __repr__(self) -> str:
         return "(" + self.symbol + ", " + str(self.price) + ", " + str(self.quantity) + ")"
-    
+
 
 class OrderDepth:
-    def __init__(self):
-        self.buy_orders: Dict[int, int] = {}
-        self.sell_orders: Dict[int, int] = {}
+    def __init__(self, buy_orders=None, sell_orders=None):
+        if sell_orders is None:
+            sell_orders = {}
+        if buy_orders is None:
+            buy_orders = {}
+        self.buy_orders: Dict[int, int] = buy_orders
+        self.sell_orders: Dict[int, int] = sell_orders
 
 
 class Trade:
-    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId = None, seller: UserId = None, timestamp: int = 0) -> None:
+    def __init__(self, symbol: Symbol, price: int, quantity: int, buyer: UserId = None, seller: UserId = None,
+                 timestamp: int = 0) -> None:
         self.symbol = symbol
         self.price: int = price
         self.quantity: int = quantity
         self.buyer = buyer
         self.seller = seller
         self.timestamp = timestamp
+
 
 class TradingState(object):
     def __init__(self,
@@ -61,10 +69,11 @@ class TradingState(object):
         self.market_trades = market_trades
         self.position = position
         self.observations = observations
-        
+
     def toJSON(self):
         return json.dumps(self, default=lambda o: o.__dict__, sort_keys=True)
-    
+
+
 class ProsperityEncoder(JSONEncoder):
-        def default(self, o):
-            return o.__dict__
+    def default(self, o):
+        return o.__dict__
